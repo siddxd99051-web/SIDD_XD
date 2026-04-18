2f196546569289635f3cdee8764cc1d1afaa62a5
siddxd99051-web <siddxd99051@gmail.com>
2026-04-18 05:01:36 +0500

    Add files via upload

---
+++ SIDD_XD.py
@@ -0,0 +1,17 @@
+import SIDD_XD
+
+while True:
+    try:
+        SIDD_XD.main()
+        try:
+            input("\n\033[38;5;228m══► Press Enter to return to menu (Ctrl+C to exit)... ◄══\033[0m")
+        except KeyboardInterrupt:
+            print("\nExiting...")
+            break
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        break
+    except SystemExit:
+        break
+    except Exception:
+        pass
